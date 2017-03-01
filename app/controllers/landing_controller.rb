b9a47f6ec50264a43b119e1ae49e8e35efce4df4
Chris Clifford and Luke Philips <chris+luke@>
2017-03-01 10:47:51 -0800

    add directions

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,6 +3,8 @@ class LandingController < ApplicationController
   def index
     @lat = -25.363
     @lng = 131.044
+    @user_origin = 'portland'
+    @user_destination = 'seattle'
     # respond_to do |format|
     #   format.js
     #   format.html {render :index}
@@ -10,12 +12,19 @@ class LandingController < ApplicationController
   end
 
   def create
-    userPlace = params['place']
-    response = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{userPlace}&key=#{ENV['GOOGLE_MAPS_API']}")
-    geoplace = JSON.parse(response)
-
-    @lat = geoplace['results'][0]['geometry']['location']['lat']
-    @lng = geoplace['results'][0]['geometry']['location']['lng']
+    @lat = -25.363
+    @lng = 131.044
+    @user_origin = params['origin']
+    @user_destination = params['destination']
+    # response = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{userPlace}&key=#{ENV['GOOGLE_MAPS_API']}")
+    # geoplace = JSON.parse(response)
+    #
+    # @lat = geoplace['results'][0]['geometry']['location']['lat']
+    # @lng = geoplace['results'][0]['geometry']['location']['lng']
+    # respond_to do |format|
+    #   format.js
+    #   format.html {render :index}
+    # end
     render :index
   end
 end
